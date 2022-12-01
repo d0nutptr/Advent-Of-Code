f02e97f95f707f31dc300e274d7fbd732dc536e2
d0nut <nathanial.lattimer@gmail.com>
2022-12-01 01:25:20 -0800

    day1

---
+++ 2022/day_1/src/problem_one.rs
@@ -0,0 +1,12 @@
+mod elves;
+
+fn main() {
+    let input = std::fs::read_to_string("input.txt")
+        .expect("missing input file");
+
+    let max_calories = elves::get_elves_and_calories(input)
+        .max()
+        .expect("No elves found...");
+
+    println!("Max calories: {max_calories}");
+}
